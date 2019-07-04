b50211e950094f34edc3e24f0d7a8fb164c72476
lhz <779196435@qq.com>
2019-07-05 00:47:49 +0800

    更新了引用资源

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var path = require('path')
 //  2.自动把打包好的 bundle.js 追加到页面中去
 //  如果要配置插件，需要在导出的对象中，挂载一个 plugins 节点
 var htmlWebpackPlugin = require('html-webpack-plugin')
-
+var VueLoaderPlugin = require('vue-loader/lib/plugin')
 // 当以命令行形式运行 webpack 或 webpack-dev-server 的时候，工具会发现，我们并没有提供 要打包 的文件的 入口 和 出口文件，此时，他会检查项目根目录中的配置文件，并读取这个文件，就拿到了导出的这个 配置对象，然后根据这个对象，进行打包构建
 module.exports = {
   entry: path.join(__dirname, './src/main.js'), // 入口文件
@@ -18,7 +18,8 @@ module.exports = {
     new htmlWebpackPlugin({//创建一个在 内存 中生成HTML页面的插件
       template: path.join(__dirname, './src/index.html'), // 指定模板文件路径
       filename: 'index.html' // 设置生成的内存页面的名称
-    })
+    }),
+    new VueLoaderPlugin()
   ],
   module: { // 配置所有第三方loader 模块的
     rules: [ // 第三方模块的匹配规则
