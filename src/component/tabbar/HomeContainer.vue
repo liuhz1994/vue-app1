320fe6e7c2a43cbb26d35f8670e63f7986225c2e
lhz <779196435@qq.com>
2019-07-06 20:44:02 +0800

    新增tabbar组件

---
+++ src/component/tabbar/HomeContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <mt-swipe :auto="4000">
+            <mt-swipe-item>1</mt-swipe-item>
+            <mt-swipe-item>2</mt-swipe-item>
+            <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="less" scoped>
+    
+</style>
+
+
+
